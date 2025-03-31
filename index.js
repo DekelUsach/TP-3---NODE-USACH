691e84ffe1a68f95fca69af27c6bbb372f5d5d1d
DEKEL USACH <48386383@ort>
2025-03-31 08:34:45 -0300

    Finished tp

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-console.log("Hola")
+// // 1
+// console.log("¡Hola, mundo!")
+// // 2
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// MostrarArray(array)
+// const MostrarArrayFlecha = (array) => console.log(array);
+// MostrarArrayFlecha(array)
+// function MostrarArray(array) {
+//     console.log(array)
+// }
+
+// // 3
+// let arrayAgregar = []
+
+// const MostrarArrayAgregar = (array) =>{
+//     array.push("hola, si")
+//     console.log(array)
+// }
+// MostrarArrayAgregar(arrayAgregar)
+
+// 4
+// const palabras = [
+//     "gato", "perro", "elefante", "computadora", "mesa", "silla", "libro", "estrella", "pescado", "cielo",
+//     "pelota", "coche", "luna", "sol", "flor", "árbol", "montaña", "río", "nube", "viento", "luz",
+//     "sola", "agua", "flor", "playa", "arena", "nieve", "hoja", "coche", "piedra", "cielo",
+//     "noche", "día", "dinosaurio", "guitarra", "película", "música", "solución", "camino", "piedra", "tiempo",
+//     "luz", "vacío", "puerta", "espejo", "sonido", "esquina", "banco", "árbol", "zorro", "ciudad",
+//     "rojo", "azul", "verde", "gris", "amarillo", "morado", "blanco", "negro", "rosa", "marrón",
+//     "lago", "desierto", "jardín", "río", "boca", "roca", "cielo", "invierno", "primavera", "verano",
+//     "otoño", "familia", "amor", "amistad", "futuro", "pasado", "presente", "mujer", "hombre", "niño",
+//     "guitarra", "piano", "batería", "violín", "trompeta", "saxo", "cello", "flauta", "clarinete", "arpa",
+//     "viaje", "aventura", "vacaciones", "escuela", "universidad", "trabajo", "dinero", "coche", "camión", "avión"
+//   ];
+
+//   console.log(palabras.lastIndexOf("piano"))
+// 5
+// const numeros = [1, 2, 3, 4, 5,10];  
+
+// const suma = numeros.reduce((acumulador, actual) => acumulador + actual, 0);
+
+// console.log(suma);
+
+// // 6
+// const array6 = [1, 2, 3, 4, 5,10];
+
+// console.log(array6.reverse())
+
+// 7
+//  const array7 = [1, 2, 3, 4, 5,10];
+
+// console.log( array7.sort((a,b) => a-b))
+
+// 8
+// let arrayFiltrar= [1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20]
+
+// const numsFiltrados = arrayFiltrar.filter(numero => numero < 15)
+
+// console.log(numsFiltrados)
+
+// 9
+// let elementoBuscado = 'uva'
+
+// let arrayFiltrar9 = ['manzana', 'plátano', 'cereza', 'uva', 'mango', 
+//              'kiwi', 'sandía', 'fresa', 'pera', 'piña', 
+//              'manzana', 'kiwi', 'plátano', 'cereza', 'pera', 
+//              'sandía', 'uva', 'mango', 'fresa', 'piña','uva'];
+
+// const numsFiltrados = arrayFiltrar9.filter(numero => numero === elementoBuscado).length
+
+//  console.log(numsFiltrados)
